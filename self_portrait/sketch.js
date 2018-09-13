4247865b55aa21c8e6c2f2745d4bfd0a9d4a8bc5
Lufire08 <locolv8@yahoo.com>
2018-09-13 12:58:36 -0400

    self portrait rough draft
    started the self portrait will add on more.

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -3,6 +3,17 @@ function setup() {
 }
 
 function draw () {
-    background ('white');
+    background ('black');
+    fill ('white');
     ellipse (320,180,200);
+    fill ('black');
+    ellipse (275,160,50);
+    ellipse (365,160,50);
+    fill ('red');
+    rect (303,220,40,10);
+    fill ('red');
+    rect (300,265,40,30);
+    rect (275,270,25,20);
+    rect (340,270,25,20);
+
 }
